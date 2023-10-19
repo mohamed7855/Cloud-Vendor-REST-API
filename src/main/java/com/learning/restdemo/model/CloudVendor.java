76b253ae66358132c2072ae3f3bfc0eb6e6d5f02
Mohamed Omara <martm7726@gmail.com>
2023-10-19 07:17:23 +0300

    add lombok

--- src/main/java/com/learning/restdemo/model/CloudVendor.java
+++ src/main/java/com/learning/restdemo/model/CloudVendor.java
@@ -1,8 +1,16 @@
 package com.learning.restdemo.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CloudVendor {
 
     @Id
@@ -10,46 +18,4 @@ public class CloudVendor {
     private String name;
     private String address;
     private String phone;
-
-    public CloudVendor() {
-    }
-
-    public CloudVendor(String id, String name, String address, String phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
